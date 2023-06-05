24d9f8afb02b27a43edca21c2898d1c816d1177e
kza-hps <kauri.tukere@gmail.com>
2023-06-05 23:30:56 +1200

    Update text_adventure_game.py
    1. Added Player.interact and Object.interact.
    2. Added more rooms.

--- text_adventure_game.py
+++ text_adventure_game.py
@@ -11,9 +11,13 @@ class Room:
         self.name = name
         self.description = description
         self.connected_rooms = {}
+        self.objects = []
 
     def connect_room(self, direction, room):
         self.connected_rooms[direction] = room
+
+    def add_object(self, obj):
+        self.objects.append(obj)
 
 class Player:
     def __init__(self):
@@ -29,7 +33,43 @@ class Player:
     def interact(self, obj):
         # code to interact with an object in the virtual world
         # you can add more details here depending on the game logic
-        pass
+        action = input(f"What do you want to do with {obj.name}? ").lower()
+        if action == "pick up":
+            if obj in self.current_room.objects:
+                self.inventory.append(obj)
+                self.current_room.objects.remove(obj)
+                print(f"You picked up {obj.name}.")
+            else:
+                print(f"You can't pick up {obj.name}.")
+        elif action == "use":
+            if obj in self.inventory:
+                # use GPT-3 to generate outcomes and consequences for using the object
+                prompt = f"You use {obj.name}. What happens?"
+                outcome = generate_response(prompt)
+                print(outcome)
+            else:
+                print(f"You don't have {obj.name}.")
+        elif action == "examine":
+            # use GPT-3 to generate descriptions for examining the object
+            prompt = f"You examine {obj.name}. What do you see?"
+            description = generate_response(prompt)
+            print(description)
+        elif action == "combine":
+            if obj in self.inventory:
+                # ask the player what other object they want to combine with
+                other_object_name = input(f"What other object do you want to combine {obj.name} with? ")
+                other_object = next((o for o in self.inventory if o.name == other_object_name), None)
+                if other_object:
+                    # use GPT-3 to generate outcomes and consequences for combining the objects
+                    prompt = f"You combine {obj.name} and {other_object.name}. What happens?"
+                    outcome = generate_response(prompt)
+                    print(outcome)
+                else:
+                    print(f"You don't have {other_object_name}.")
+            else:
+                print(f"You don't have {obj.name}.")
+        else:
+            print("I don't understand that action.")
 
 class VirtualWorld:
     def __init__(self):
@@ -76,17 +116,37 @@ def play_game():
     # Set up the virtual world
     room1 = Room("Room 1", "A dimly lit room.")
     room2 = Room("Room 2", "A room with a large window.")
+    room3 = Room("Room 3", "A room with a locked door.")
+    
     room1.connect_room("north", room2)
     room2.connect_room("south", room1)
-    
+    room2.connect_room("east", room3)
+    room3.connect_room("west", room2)
+
     world.add_room(room1)
     world.add_room(room2)
+    world.add_room(room3)
+
+    key = Object("key", "A small metal key.")
+    knife = Object("knife", "A sharp knife.")
+    door = Object("door", "A wooden door with a keyhole.")
+
+    room1.add_object(key)
+    room2.add_object(knife)
+    room3.add_object(door)
+
+    world.add_object(key)
+    world.add_object(knife)
+    world.add_object(door)
     
     player.current_room = room1  # Player starts in room1
 
     while True:
         print("You are in", player.current_room.name)
         print(generate_description(player.current_room))
+        print("You see the following objects in this room:")
+        for obj in player.current_room.objects:
+            print("-", obj.name)
         
         command = input("> ").split()
         
@@ -98,13 +158,13 @@ def play_game():
                 print("Which direction do you want to move?")
         elif action == "interact":
             if len(command) > 1:
-                # Assuming the object is in the same room as the player
+                # Assuming the object is in the same room as the player or in the player's inventory
                 object_name = " ".join(command[1:])
                 obj = next((o for o in world.objects if o.name == object_name), None)
                 if obj:
                     player.interact(obj)
                 else:
-                    print("That object doesn't exist in this room.")
+                    print("That object doesn't exist in this room or in your inventory.")
             else:
                 print("What do you want to interact with?")
         else:
